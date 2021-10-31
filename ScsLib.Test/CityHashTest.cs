b6b3ad1f838c4e6c92da0d7fea708614f1b6ae38
Jonas Houben <jonashoubenring@gmail.com>
2021-10-31 19:05:08 +0100

    added test injection

--- ScsLib.Test/CityHashTest.cs
+++ ScsLib.Test/CityHashTest.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ScsLib.Hashing;
 
 namespace ScsLib.Test
 {
 	[TestClass]
-	public class CityHashTest
+	public class CityHashTest : TestBase
 	{
 		[DataTestMethod]
 		[DataRow("", 11160318154034397263UL)]
@@ -15,7 +16,9 @@ namespace ScsLib.Test
 		[DataRow("326947f5-a3b2-48eb-904b-b97c529f9f3f425df140-7968-46f4-bd4b-db33a4f652aa11ef3fc1-8433-4710-86d1-ab6effbdbfd1", 10176022315815290723UL)]
 		public void CityHash64_ReturnCorrectHash(string data, ulong expectedHash)
 		{
-			ulong hash = new CityHash().CityHash64(data);
+			ICityHash cityHash = ServiceProvider.GetRequiredService<ICityHash>();
+
+			ulong hash = cityHash.CityHash64(data);
 
 			Assert.AreEqual(expectedHash, hash);
 		}
---
+++ ScsLib.Test/TestBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ScsLib.Test
+{
+	public abstract class TestBase
+	{
+		public IServiceProvider ServiceProvider { get; private set; } = default!;
+
+		[TestInitialize]
+		public void TestInit()
+		{
+			ServiceCollection services = new ServiceCollection();
+
+			services.AddScsLib();
+
+			ServiceProvider = services.BuildServiceProvider();
+		}
+	}
+}
