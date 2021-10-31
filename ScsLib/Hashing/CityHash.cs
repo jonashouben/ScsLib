790b97acf25711a9ef0d3bd1aaf1230d55d6c99a
Jonas Houben <jonashoubenring@gmail.com>
2021-10-31 13:07:56 +0100

    CityHash Span rework

--- ScsLib/Hashing/CityHash.cs
+++ ScsLib/Hashing/CityHash.cs
@@ -39,14 +39,14 @@ namespace ScsLib.Hashing
 			return val ^ (val >> 47);
 		}
 
-		private static uint Fetch32(byte[] data, int pos = 0)
+		private static uint Fetch32(ReadOnlySpan<byte> data)
 		{
-			return BitConverter.ToUInt32(data, pos);
+			return BitConverter.ToUInt32(data);
 		}
 
-		private static ulong Fetch64(byte[] data, int pos = 0)
+		private static ulong Fetch64(ReadOnlySpan<byte> data)
 		{
-			return BitConverter.ToUInt64(data, pos);
+			return BitConverter.ToUInt64(data);
 		}
 
 		private static ulong Hash128To64((ulong, ulong) x)
@@ -59,19 +59,19 @@ namespace ScsLib.Hashing
 			return b;
 		}
 
-		private static ulong HashLen0To16(byte[] s, int len)
+		private static ulong HashLen0To16(ReadOnlySpan<byte> s, int len)
 		{
 			if (len > 8)
 			{
 				ulong a = Fetch64(s);
-				ulong b = Fetch64(s, len - 8);
+				ulong b = Fetch64(s.Slice(len - 8));
 				return Hash128To64((a, RotateByAtleast1(b + (ulong)len, len))) ^ b;
 			}
 
 			if (len >= 4)
 			{
 				ulong a = Fetch32(s);
-				return Hash128To64(((ulong)len + (a << 3), Fetch32(s, len - 4)));
+				return Hash128To64(((ulong)len + (a << 3), Fetch32(s.Slice(len - 4))));
 			}
 
 			if (len > 0)
@@ -87,34 +87,34 @@ namespace ScsLib.Hashing
 			return K2;
 		}
 
-		private static ulong HashLen17To32(byte[] s, int len)
+		private static ulong HashLen17To32(ReadOnlySpan<byte> s, int len)
 		{
 			ulong a = Fetch64(s) * K1;
-			ulong b = Fetch64(s, 8);
-			ulong c = Fetch64(s, len - 8) * K2;
-			ulong d = Fetch64(s, len - 16) * K0;
+			ulong b = Fetch64(s.Slice(8));
+			ulong c = Fetch64(s.Slice(len - 8)) * K2;
+			ulong d = Fetch64(s.Slice(len - 16)) * K0;
 			return Hash128To64((Rotate(a - b, 43) + Rotate(c, 30) + d,
 							 a + Rotate(b ^ K3, 20) - c + (ulong)len));
 		}
 
-		private static ulong HashLen33To64(byte[] s, int len)
+		private static ulong HashLen33To64(ReadOnlySpan<byte> s, int len)
 		{
-			ulong z = Fetch64(s, 24);
-			ulong a = Fetch64(s) + (((ulong)len + Fetch64(s, len - 16)) * K0);
+			ulong z = Fetch64(s.Slice(24));
+			ulong a = Fetch64(s) + (((ulong)len + Fetch64(s.Slice(len - 16))) * K0);
 			ulong b = Rotate(a + z, 52);
 			ulong c = Rotate(a, 37);
-			a += Fetch64(s, 8);
+			a += Fetch64(s.Slice(8));
 			c += Rotate(a, 7);
-			a += Fetch64(s, 16);
+			a += Fetch64(s.Slice(16));
 			ulong vf = a + z;
 			ulong vs = b + Rotate(a, 31) + c;
-			a = Fetch64(s, 16) + Fetch64(s, len - 32);
-			z = Fetch64(s, len - 8);
+			a = Fetch64(s.Slice(16)) + Fetch64(s.Slice(len - 32));
+			z = Fetch64(s.Slice(len - 8));
 			b = Rotate(a + z, 52);
 			c = Rotate(a, 37);
-			a += Fetch64(s, len - 24);
+			a += Fetch64(s.Slice(len - 24));
 			c += Rotate(a, 7);
-			a += Fetch64(s, len - 16);
+			a += Fetch64(s.Slice(len - 16));
 			ulong wf = a + z;
 			ulong ws = b + Rotate(a, 31) + c;
 			ulong r = ShiftMix(((vf + ws) * K2) + ((wf + vs) * K0));
@@ -133,26 +133,25 @@ namespace ScsLib.Hashing
 			return (a + z, b + c);
 		}
 
-		private static (ulong, ulong) WeakHashLen32WithSeeds(byte[] s, int len, ulong a, ulong b)
+		private static (ulong, ulong) WeakHashLen32WithSeeds(ReadOnlySpan<byte> s, int len, ulong a, ulong b)
 		{
-			return WeakHashLen32WithSeeds(Fetch64(s, len),
-										  Fetch64(s, len + 8),
-										  Fetch64(s, len + 16),
-										  Fetch64(s, len + 24),
+			return WeakHashLen32WithSeeds(Fetch64(s.Slice(len)),
+										  Fetch64(s.Slice(len + 8)),
+										  Fetch64(s.Slice(len + 16)),
+										  Fetch64(s.Slice(len + 24)),
 										  a,
 										  b);
 		}
-		#endregion
 
-		private static ulong CityHash64(byte[] s, int len)
+		private static ulong CityHash64(ReadOnlySpan<byte> s, int len)
 		{
 			if (len <= 16) return HashLen0To16(s, len);
 			if (len <= 32) return HashLen17To32(s, len);
 			if (len <= 64) return HashLen33To64(s, len);
 
-			ulong x = Fetch64(s, len - 40);
-			ulong y = Fetch64(s, len - 16) + Fetch64(s, len - 56);
-			ulong z = Hash128To64((Fetch64(s, len - 48) + (ulong)len, Fetch64(s, len - 24)));
+			ulong x = Fetch64(s.Slice(len - 40));
+			ulong y = Fetch64(s.Slice(len - 16)) + Fetch64(s.Slice(len - 56));
+			ulong z = Hash128To64((Fetch64(s.Slice(len - 48)) + (ulong)len, Fetch64(s.Slice(len - 24))));
 			var v = WeakHashLen32WithSeeds(s, len - 64, (ulong)len, z);
 			var w = WeakHashLen32WithSeeds(s, len - 32, y + K1, x);
 			x = (x * K1) + Fetch64(s);
@@ -161,13 +160,13 @@ namespace ScsLib.Hashing
 			len = (len - 1) & ~63;
 			do
 			{
-				x = Rotate(x + y + v.Item1 + Fetch64(s, pos + 8), 37) * K1;
-				y = Rotate(y + v.Item2 + Fetch64(s, pos + 48), 42) * K1;
+				x = Rotate(x + y + v.Item1 + Fetch64(s.Slice(pos + 8)), 37) * K1;
+				y = Rotate(y + v.Item2 + Fetch64(s.Slice(pos + 48)), 42) * K1;
 				x ^= w.Item2;
-				y += v.Item1 + Fetch64(s, pos + 40);
+				y += v.Item1 + Fetch64(s.Slice(pos + 40));
 				z = Rotate(z + w.Item1, 33) * K1;
 				v = WeakHashLen32WithSeeds(s, pos, v.Item2 * K1, x + w.Item1);
-				w = WeakHashLen32WithSeeds(s, pos + 32, z + w.Item2, y + Fetch64(s, pos + 16));
+				w = WeakHashLen32WithSeeds(s, pos + 32, z + w.Item2, y + Fetch64(s.Slice(pos + 16)));
 				Swap(ref z, ref x);
 
 				pos += 64;
@@ -178,5 +177,6 @@ namespace ScsLib.Hashing
 			return Hash128To64((Hash128To64((v.Item1, w.Item1)) + (ShiftMix(y) * K1) + z,
 							 Hash128To64((v.Item2, w.Item2)) + x));
 		}
+		#endregion
 	}
 }
