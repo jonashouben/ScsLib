bfe2f0ed4fbd668d94f3fe931ddf45b6b964a519
Jonas Houben <jonashoubenring@gmail.com>
2021-11-01 11:43:56 +0100

    changed to span for directory reading

--- ScsLib/HashFileSystem/Reader/HashDirectoryReader.cs
+++ ScsLib/HashFileSystem/Reader/HashDirectoryReader.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -19,31 +17,68 @@ namespace ScsLib.HashFileSystem.Reader
 
 		public async Task<IReadOnlyCollection<HashDirectoryEntry>> ReadAsync(Stream stream, HashDirectory hashDirectory, CancellationToken cancellationToken = default)
 		{
-			return await _ReadAsync(stream, hashDirectory, cancellationToken).ToArrayAsync(cancellationToken).ConfigureAwait(false);
-		}
-
-		private async IAsyncEnumerable<HashDirectoryEntry> _ReadAsync(Stream stream, HashDirectory hashDirectory, [EnumeratorCancellation] CancellationToken cancellationToken)
-		{
 			string directoryString = await _hashEntryReader.ReadStringAsync(stream, hashDirectory, cancellationToken).ConfigureAwait(false);
 
-			foreach (ReadOnlySpan<char> content in directoryString.Split('\n', StringSplitOptions.RemoveEmptyEntries))
+			return _Read(directoryString);
+		}
+
+		private IReadOnlyCollection<HashDirectoryEntry> _Read(ReadOnlySpan<char> directoryString)
+		{
+			List<HashDirectoryEntry> entries = new List<HashDirectoryEntry>();
+
+			foreach (ReadOnlySpan<char> content in new LineSplitEnumerator(directoryString))
 			{
 				if (content.StartsWith("*", StringComparison.Ordinal))
 				{
-					yield return new HashDirectoryEntry
+					entries.Add(new HashDirectoryEntry
 					{
 						Type = HashDirectoryEntryType.Directory,
 						Name = content.Slice(1).ToString()
-					};
+					});
 				}
 				else
 				{
-					yield return new HashDirectoryEntry
+					entries.Add(new HashDirectoryEntry
 					{
 						Type = HashDirectoryEntryType.File,
 						Name = content.ToString()
-					};
+					});
 				}
+			}
+
+			return entries;
+		}
+
+		private ref struct LineSplitEnumerator
+		{
+			private readonly ReadOnlySpan<char> _input;
+			private int _pos;
+
+			public ReadOnlySpan<char> Current { get; private set; }
+
+			public LineSplitEnumerator(ReadOnlySpan<char> input)
+			{
+				_input = input;
+				_pos = 0;
+				Current = default;
+			}
+
+			public LineSplitEnumerator GetEnumerator() => this;
+
+			public bool MoveNext()
+			{
+				if (_pos >= _input.Length) return false;
+
+				int startPos = _pos;
+
+				while (_pos < _input.Length && _input[_pos] != '\n')
+				{
+					_pos++;
+				}
+
+				Current = _input.Slice(startPos, _pos - startPos);
+				_pos++;
+				return true;
 			}
 		}
 	}
